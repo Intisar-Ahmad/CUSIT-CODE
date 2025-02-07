a944a5340741fc4618f4db9258eede56c6281e5f
Intisar-Ahmad <intisar12122000@gmail.com>
2025-02-07 23:40:11 +0500

    Fixed an issue

--- Final Paper Preparation/problem1.cpp
+++ Final Paper Preparation/problem1.cpp
@@ -5,17 +5,23 @@ using namespace std;
 int main(){
 
     //How to find the second largest number in an array
-    int arr[10] = {9,6,4,7,3,6,8,1,0,5};
+    int arr[5] = {9,6,4,7,3};
     int maxNumber = 0;//Initialize with the lowest numbr possible
     int secMax = 0;//Same here
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 5; i++)
     {
+
+        // max = 9
+
         if(arr[i] > maxNumber){//Check if arr[i] is greater than the maxNumber
             secMax = maxNumber;//If true assign the value of the previous largest Number to secMax
             maxNumber = arr[i];//Update the maxNumber
         }
+       else if(arr[i] > secMax && arr[i] < maxNumber){
+            secMax = arr[i];
+        }
     }
     //You can ask chatGPT for further explanation
-    cout<<secMax<<maxNumber<<endl;
+    cout<<"Second Largest "<<secMax<<" Largest "<<maxNumber<<endl;
     return 0;
 }
