252d7d00ae72a4211af31564c001d2e69641bf72
Intisar-Ahmad <intisar12122000@gmail.com>
2025-11-16 19:06:44 +0500

    Major Assignment Fahad Zia

---
+++ Cpp/3rd Sem/DSA(Lab)/Major Assignment(Fahad Zia)/main.cpp
@@ -0,0 +1,305 @@
+#include <iostream>
+#include<vector>
+#include<limits>
+
+using namespace std;
+
+
+// Item class(Node for Items Class)
+class Item
+{
+public:
+    string name;
+    Item *next;
+    Item *prev;
+    Item(string name) : name(name)
+    {
+        next = nullptr;
+        prev = nullptr;
+    }
+};
+
+// list of items
+class Items
+{
+public:
+    // constructor
+    Item* head = nullptr;
+    Item* tail = nullptr;
+
+
+    // Insert at end
+    void append(string name){
+        Item* newItem = new Item(name);
+        if(!head){
+            head = newItem;
+            tail = newItem;
+        }
+        else{
+            tail->next = newItem;
+            newItem->prev = tail;
+            tail = newItem;
+        }
+    }
+    
+    // display Items
+    void display(){
+        if(!head){
+            cout << "List is empty\n";
+            return;
+        }
+        Item* temp = head;
+        while(temp){
+            cout << temp->name << "->";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+};
+
+// orderPackage (contains lists of Items)
+class orderPackage{
+    public:
+    Items list;
+    string shopName;
+    float distance;
+    string address;
+
+    orderPackage(){}
+    
+    orderPackage(Items list,string shopName,string address,float distance):list(list),shopName(shopName),address(address),distance(distance){}
+
+
+
+    // display 
+    void display(){
+        cout << "Items:";
+        list.display();
+        cout << shopName << endl;
+        cout << distance << endl;
+        cout << address << endl;
+    }
+};
+
+// bag stack
+class bagStack{
+    public:
+    orderPackage orders[3];
+    int top = -1;
+
+
+    // push
+    void push(orderPackage order){
+        if(top == 2){
+            cout << "Stack overflow\n";
+            return;
+        }
+        orders[++top] = order;
+    }
+
+    //isEmpty
+    bool isEmpty(){
+         if(top == -1){
+            return true;
+        }
+        return false;
+    }
+
+    // pop
+       orderPackage pop(){
+        if(isEmpty()){
+            cout << "Stack underflow\n";
+            return orderPackage();
+        }
+       return orders[top--];
+    }
+};
+
+// Nodes for conveyer belt
+class bagNode{
+    public:
+    bagStack bag;
+    bagNode* next = nullptr;
+    bagNode(bagStack bag):bag(bag){}
+};
+
+// conveyer belt queue
+class conveyerBeltQueue{
+    public:
+    bagNode* front = nullptr;
+    bagNode* rear = nullptr;
+
+    // enqueue
+    void enqueue(bagStack bag){
+        bagNode* newBag = new bagNode(bag);
+        if(isEmpty()){
+            front = newBag;
+            rear = newBag;
+        }
+        else{
+            rear->next = newBag;
+            rear = newBag;
+        }
+    }
+    
+    // dequeue
+    bagStack dequeue(){
+        if(isEmpty()){
+            cout << "Queue is empty\n";
+            return bagStack();
+        }
+        bagNode* temp = front;
+        front = front->next;
+        bagStack bag = temp->bag;
+        delete temp;
+        return bag;
+    }
+
+    // isEmpty
+    bool isEmpty(){
+        if(!front && !rear){
+            return true;
+        }
+        return false;
+    }
+
+};
+
+
+// merging of Arrays
+void merge(vector<orderPackage>& orders,int leftSt,int leftEnd, int rightEnd){
+    vector<orderPackage> temp;
+    int i = leftSt;
+    int j = leftEnd + 1;
+    while(i <= leftEnd && j <= rightEnd){
+        if(orders[i].distance > orders[j].distance){
+            temp.push_back(orders[i++]);
+        }
+        else{
+            temp.push_back(orders[j++]);
+        }
+    }
+
+    while(i <= leftEnd){
+        temp.push_back(orders[i++]);
+    }
+
+    while(j <= rightEnd){
+        temp.push_back(orders[j++]);
+    }
+   
+    // filling the original array
+    i = leftSt;
+    j = 0;
+    int size = temp.size();
+    while(i <= rightEnd && j < size){
+        orders[i++] = temp[j++];
+    }
+
+
+}
+
+// mergeSort
+void mergeSort(vector<orderPackage>& orders, int st, int end){
+    if(st < end){
+        int mid = st + (end - st)/2;
+        
+        // Left half
+        mergeSort(orders,st,mid);
+        
+        // Right half
+        mergeSort(orders,mid + 1,end);
+
+        // merging
+        merge(orders,st,mid,end);
+        
+    }
+}
+
+int main()
+{
+
+    vector<orderPackage> orders;
+    int isRunning = 1;
+    conveyerBeltQueue belt;
+
+    while(isRunning){
+        string name;
+        string address;
+        Items list;
+        float distance;
+
+
+        cout << "Enter your details:\n";
+
+        cout << "Enter shop name:";
+        // cin.ignore(1, '\n'); 
+        getline(cin,name);
+
+        cout << "Enter Address:";
+        // cin.ignore(1, '\n'); 
+        getline(cin,address);
+
+        cout << "Enter Distance:";
+        cin >> distance;
+
+        cout << "Enter Items:\n";
+        int ordering = 1;
+        while(ordering){
+            string itemName;
+            cout << "Enter item name:\n";
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+            getline(cin,itemName);
+            list.append(itemName);
+            cout << "Press 0 to exit and 1 to stay:";
+            cin >> ordering;
+        }
+
+        orderPackage order(list,name,address,distance);
+        order.display();
+        cout << endl << endl;
+        
+        orders.push_back(order);
+        
+        
+
+        cout<< "Enter 1 to continue ordering and 0 to see results:";
+        cin >> isRunning;
+        cin.ignore(10,'\n');
+    
+    }
+
+    // sort the vector
+    mergeSort(orders,0,orders.size() - 1);
+    int sizeOfOrders = orders.size();
+
+    // adding items to belt
+    for (int i = 0; i < sizeOfOrders; ) {
+    bagStack bag;
+    int counter = 0;
+    while(counter < 3 && i < sizeOfOrders) {
+        bag.push(orders[i]);
+        i++;
+        counter++;
+    }
+    belt.enqueue(bag);
+}
+
+    
+
+    // dequeueing
+    cout << endl << endl << "Delivering Orders:\n\n";
+    while(!belt.isEmpty()){
+        cout << "Order 1\n\n";
+        bagStack bag = belt.dequeue();
+        while(!bag.isEmpty()){
+            orderPackage order = bag.pop();
+            order.display();
+            cout << endl << endl;
+        }
+        cout << "Order Complete\n\n";
+    }
+    
+    
+    return 0;
+}
